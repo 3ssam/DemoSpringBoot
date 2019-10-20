ced4e0d35b73ae30525bc07c65f40760a19d8a89
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-20 15:11:14 +0200

    Create beans from object by using @Component by default pattern that is singleton that is create one instance from object when run start application

--- src/main/java/mo/essam/DemospringbootApplication.java
+++ src/main/java/mo/essam/DemospringbootApplication.java
@@ -1,13 +1,18 @@
 package mo.essam;
 
+import mo.essam.models.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemospringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemospringbootApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(DemospringbootApplication.class, args);
+        context.getBean(Student.class).justMessage();
+        context.getBean(Student.class).justMessage();
+        context.getBean(Student.class).justMessage();
     }
 
 }
